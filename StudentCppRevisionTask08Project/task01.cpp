00fbb3ee5e32a8b1580dc9d12545acd72ee552da
valentinaivanova1910 <125904818+valentinaivanova1910@users.noreply.github.com>
2023-05-02 18:06:47 +0300

    task

--- StudentCppRevisionTask08Project/task01.cpp
+++ StudentCppRevisionTask08Project/task01.cpp
@@ -25,5 +25,24 @@
  */
 
 int task01(int a, int b, int c, int d) {
-	return 0;
+	if (a == b && b == c && c == d && d == a) {
+		return 4;
+	}
+	else if (a == b && a == c && c == b && d == a||d==b||d==c) {
+		return 3;
+	}
+	else if ( a == b && b = c && c != d && d != a) {
+		return 2;
+	}
+	else if  (a != b && b != c && c != d && d != a) {
+		return 1;
+	}
+	else  (a != b && b != c && c != d && d != a) {
+		return 0;
+	}
+	
+
 }
+/*if (a / b == 1)num++;
+	if (b / c == 1)num++;
+	if (c / d == 1)num++;*/
